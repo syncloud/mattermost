3da36f5e4821c402e6ff0a7805dc316580c348b1
Boris Rybalkin <ribalkin@gmail.com>
2024-09-11 07:40:36 +0100

    build

--- mattermost/test.sh
+++ mattermost/test.sh
@@ -3,9 +3,5 @@
 DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 
-BUILD_DIR=${DIR}/../build/snap/paperless
-$BUILD_DIR/sbin/python --version
-$BUILD_DIR/sbin/python ${BUILD_DIR}/usr/local/bin/celery --version
-$BUILD_DIR/sbin/tesseract --list-langs | grep eng
-$BUILD_DIR/sbin/tesseract --list-langs
-$BUILD_DIR/sbin/convert --version
+BUILD_DIR=${DIR}/../build/snap/mattermost
+$BUILD_DIR/bin/mattermost --version
