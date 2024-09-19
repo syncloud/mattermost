147df8ae4ce902fc5043103b2dd560880e70cca4
Boris Rybalkin <ribalkin@gmail.com>
2024-09-19 21:08:40 +0100

    wait for configure

--- bin/service.mattermost.sh
+++ bin/service.mattermost.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 /bin/rm -f $SNAP_COMMON/web.socket
 source $SNAP_DATA/config/mattermost.env
+$DIR/wait-for-configure.sh
 exec $SNAP/mattermost/sbin/mattermost -c $SNAP_DATA/config/config.json
