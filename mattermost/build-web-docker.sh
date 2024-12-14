31c8b4c0d78c1caad3383da411f8983e0d4ad246
Boris Rybalkin <ribalkin@gmail.com>
2024-12-14 18:30:09 +0000

    dir

--- mattermost/build-web-docker.sh
+++ mattermost/build-web-docker.sh
@@ -8,9 +8,10 @@ while ! docker create --name=app mattermost/mattermost-enterprise-edition:$VERSI
   sleep 1
   echo "retry docker"
 done
-mkdir -p ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}/mattermost
 cd ${DIR}/../build
 docker export app -o app.tar
 tar xf app.tar
 rm -rf app.tar
-cp -R mattermost/client ${BUILD_DIR}/
+cp -R mattermost/templates ${BUILD_DIR}/mattermost
+cp -R mattermost/client ${BUILD_DIR}/mattermost
