fc5116b275ac528cee434be8177abf705e4bd0b4
Boris Rybalkin <ribalkin@gmail.com>
2024-12-24 10:18:20 +0000

    tear down

--- test/test.py
+++ test/test.py
@@ -49,7 +49,7 @@ def module_setup(request, device, app_dir, artifact_dir):
     request.addfinalizer(module_teardown)
 
 
-def test_start(device, device_host, app, domain):
+def test_start(module_setup, device, device_host, app, domain):
     add_host_alias(app, device_host, domain)
     device.run_ssh('date', retries=10)
     device.run_ssh('mkdir {0}'.format(TMP_DIR))
