98b671afdff25011b97fc45ccd753195012b8d9a
Boris Rybalkin <ribalkin@gmail.com>
2025-10-05 09:37:04 +0100

    ui test

--- test/ui.py
+++ test/ui.py
@@ -30,6 +30,12 @@ def test_start(module_setup, app, domain, device_host):
     add_host_alias(app, device_host, domain)
 
 
+@pytest.mark.flaky(retries=10, delay=5)
+def test_visible_through_platform(app_domain):
+    response = requests.get('https://{0}'.format(app_domain), verify=False)
+    assert response.status_code == 200, response.text
+
+
 def test_login(selenium, device_user, device_password):
     selenium.open_app()
     selenium.find_by(By.XPATH, "//span[contains(.,'View in Browser')]").click()
