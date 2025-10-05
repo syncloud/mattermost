fe28d6e8651df75bb3704d486fc6f8101fecbd28
Boris Rybalkin <ribalkin@gmail.com>
2025-10-05 12:31:14 +0100

    requests

--- test/ui.py
+++ test/ui.py
@@ -3,6 +3,7 @@ from os.path import dirname, join
 from subprocess import check_output
 
 import pytest
+import requests
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from syncloudlib.integration.hosts import add_host_alias
