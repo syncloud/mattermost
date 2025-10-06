4ff7fc4bd394fd9dc85cb11990c46559d1161e6e
Boris Rybalkin <ribalkin@gmail.com>
2025-10-06 07:03:04 +0100

    close

--- test/ui.py
+++ test/ui.py
@@ -64,8 +64,9 @@ def test_login(selenium, device_user, device_password):
 
     selenium.find_by(By.XPATH, "//span[contains(.,'Experience a better way')]")
     selenium.screenshot('experience')
-    selenium.click_by(By.XPATH, "//span[contains(.,'Experience a better way')]/../../../..//span'")
-
+    #selenium.click_by(By.XPATH, "//span[contains(.,'Experience a better way')]/../../../..//span'")
+    selenium.click_by(By.XPATH, "//button[@class='close']")
+   
     selenium.find_by(By.XPATH, "//span[.='Visible to Admins only']")
     selenium.screenshot('welcome')
     selenium.click_by(By.XPATH, "//button[@class='close']")
