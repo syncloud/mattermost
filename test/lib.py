cdfeec9ef1bbb9a5a32b8bd4e89bf5da1c7c09ad
Boris Rybalkin <ribalkin@gmail.com>
2025-10-11 11:12:23 +0100

    prev next

--- test/lib.py
+++ test/lib.py
@@ -45,9 +45,10 @@ def login_prev(mode, selenium, device_user, device_password):
     #selenium.click_by(By.XPATH, "//span[contains(.,'Experience a better way')]/../../../..//span'")
     #selenium.click_by(By.XPATH, "//button[@class='close']")
     #selenium.invisible_by(By.XPATH, "//span[contains(.,'Experience a better way')]")
-    selenium.invisible_by(By.XPATH, "//span[.='Visible to Admins only']")
-    selenium.click_by(By.XPATH, "//span[contains(.,'No thanks')]")
-
+    selenium.find_by(By.XPATH, "//span[contains(.,'Visible to Admins only')]")
+    selenium.click_by(By.XPATH, "//span[contains(.,'Next')]")
+    selenium.invisible_by(By.XPATH, "//span[contains(.,'Visible to Admins only')]")
+ 
     selenium.find_by(By.XPATH, "//span[contains(.,'Welcome to Town Square')]")
     #selenium.screenshot(mode+'-welcome')
     #selenium.click_by(By.XPATH, "//button[@class='close']")
