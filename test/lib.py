5be799b225bab7f89100182574f66a9d23d0a97e
Boris Rybalkin <ribalkin@gmail.com>
2025-12-21 23:13:21 +0000

    welcome test

--- test/lib.py
+++ test/lib.py
@@ -10,63 +10,6 @@ from syncloudlib.integration.hosts import add_host_alias
 
 
 def login_prev(mode, selenium, device_user, device_password):
-    selenium.find_by(By.XPATH, "//span[contains(.,'View in Desktop')]")
-    selenium.find_by(By.XPATH, "//span[contains(.,'View in Browser')]")
-    selenium.screenshot(mode+'-view')
-    selenium.click_by(By.XPATH, "//span[contains(.,'View in Browser')]")
-    selenium.invisible_by(By.XPATH, "//span[contains(.,'View in Browser')]")
-    selenium.screenshot(mode+'-ldap')
-    selenium.click_by(By.XPATH, "//span[contains(.,'LDAP Credential')]")
-    selenium.find_by(By.ID, "input_loginId").send_keys(device_user)
-    password = selenium.find_by(By.ID, "input_password-input")
-    password.send_keys(device_password)
-    selenium.screenshot(mode+'-login')
-    #password.send_keys(Keys.RETURN)
-    #selenium.find_by(By.ID, "sign-in-button").click()
-    #selenium.find_by(By.ID, "accept-button").click()
-    selenium.find_by(By.XPATH, "//span[contains(.,'Log in')]").click()
-    selenium.find_by(By.XPATH, "//input[@placeholder='Organization name']").send_keys("testorg")
-    selenium.screenshot(mode+'-org')
-    selenium.click_by(By.XPATH, "//span[.='Continue']")
-
-    selenium.find_by(By.XPATH, "//span[.='What tools do you use?']")
-    selenium.find_by(By.XPATH, "//span[.='GitHub']")
-    selenium.find_by(By.XPATH, "//span[.='GitLab']")
-    selenium.find_by(By.XPATH, "//span[.='Skip']")
-    selenium.screenshot(mode+'-tools')
-    selenium.click_by(By.XPATH, '//span[.="What tools do you use?"]/../..//span[.="Continue"]')
-    selenium.invisible_by(By.XPATH, "//span[.='What tools do you use?']")
-    # selenium.click_by(By.XPATH, "//span[.='Continue']")
-
-    selenium.find_by(By.XPATH, "//span[.='Invite your team members']")
-    selenium.screenshot(mode+'-invite')
-    selenium.click_by(By.XPATH, "//span[.='Finish setup']")
-    selenium.invisible_by(By.XPATH, "//span[.='Invite your team members']")
-
-    selenium.find_by(By.XPATH, "//span[contains(.,'Visible to Admins only')]")
-    selenium.click_by(By.XPATH, "//span[contains(.,'Next')]")
-    selenium.invisible_by(By.XPATH, "//span[contains(.,'Visible to Admins only')]")
- 
-    selenium.find_by(By.XPATH, "//span[contains(.,'Experience a better way')]")
-    selenium.screenshot(mode+'-experience')
-    #selenium.find_by(By.XPATH, "//span[contains(.,'Experience a better way')]/../../../..//span")
-    selenium.click_by(By.XPATH, "//button[@class='close']")
-    selenium.invisible_by(By.XPATH, "//span[contains(.,'Experience a better way')]")
-
-    selenium.find_by(By.XPATH, "//span[contains(.,'Welcome to Town Square')]")
-    #selenium.screenshot(mode+'-welcome')
-    #selenium.click_by(By.XPATH, "//button[@class='close']")
-    #selenium.invisible_by(By.XPATH, "//span[.='Visible to Admins only']")
-    
-    if selenium.exists_by(By.XPATH, "//span[contains(.,'No thanks')]"):
-        selenium.screenshot(mode+'-no-thanks')
-        selenium.click_by(By.XPATH, "//span[contains(.,'No thanks')]")
-        selenium.invisible_by(By.XPATH, "//span[contains(.,'No thanks')]")
-
-    selenium.screenshot(mode+'-chat')
-
-
-def login_next(mode, selenium, device_user, device_password):
     selenium.find_by(By.XPATH, "//span[contains(.,'View in Browser')]").click()
     selenium.invisible_by(By.XPATH, "//span[contains(.,'View in Browser')]")
     selenium.screenshot(mode+'-ldap')
@@ -109,6 +52,53 @@ def login_next(mode, selenium, device_user, device_password):
     #selenium.invisible_by(By.XPATH, "//span[.='Visible to Admins only']")
     #selenium.click_by(By.XPATH, "//span[contains(.,'No thanks')]")
     selenium.invisible_by(By.XPATH, "//span[contains(.,'No thanks')]")
+
+    selenium.screenshot(mode+'-chat')
+
+
+def login_next(mode, selenium, device_user, device_password):
+    selenium.find_by(By.XPATH, "//span[contains(.,'View in Browser')]").click()
+    selenium.invisible_by(By.XPATH, "//span[contains(.,'View in Browser')]")
+    selenium.screenshot(mode+'-ldap')
+    selenium.click_by(By.XPATH, "//span[contains(.,'LDAP Credential')]")
+    selenium.find_by(By.ID, "input_loginId").send_keys(device_user)
+    password = selenium.find_by(By.ID, "input_password-input")
+    password.send_keys(device_password)
+    selenium.screenshot(mode+'-login')
+    #password.send_keys(Keys.RETURN)
+    #selenium.find_by(By.ID, "sign-in-button").click()
+    #selenium.find_by(By.ID, "accept-button").click()
+    selenium.find_by(By.XPATH, "//span[contains(.,'Log in')]").click()
+    selenium.find_by(By.XPATH, "//input[@placeholder='Organization name']").send_keys("testorg")
+    selenium.screenshot(mode+'-org')
+    selenium.click_by(By.XPATH, "//span[.='Continue']")
+
+    selenium.find_by(By.XPATH, "//span[.='What tools do you use?']")
+    selenium.find_by(By.XPATH, "//span[.='GitHub']")
+    selenium.find_by(By.XPATH, "//span[.='GitLab']")
+    selenium.find_by(By.XPATH, "//span[.='Skip']")
+    selenium.screenshot(mode+'-tools')
+    selenium.click_by(By.XPATH, '//span[.="What tools do you use?"]/../..//span[.="Continue"]')
+    selenium.invisible_by(By.XPATH, "//span[.='What tools do you use?']")
+    # selenium.click_by(By.XPATH, "//span[.='Continue']")
+
+    selenium.find_by(By.XPATH, "//span[.='Invite your team members']")
+    selenium.screenshot(mode+'-invite')
+    selenium.click_by(By.XPATH, "//span[.='Finish setup']")
+    selenium.invisible_by(By.XPATH, "//span[.='Invite your team members']")
+
+    #selenium.find_by(By.XPATH, "//span[contains(.,'Experience a better way')]")
+    #selenium.screenshot(mode+'-experience')
+    #selenium.click_by(By.XPATH, "//span[contains(.,'Experience a better way')]/../../../..//span'")
+    #selenium.click_by(By.XPATH, "//button[@class='close']")
+    #selenium.invisible_by(By.XPATH, "//span[contains(.,'Experience a better way')]")
+ 
+    selenium.find_by(By.XPATH, "//span[.='Mattermost 11.1 is here!']")
+    selenium.screenshot(mode+'-welcome')
+    selenium.click_by(By.XPATH, "//button[@class='close']")
+    selenium.invisible_by(By.XPATH, "//span[.='Visible to Admins only']")
+    #selenium.click_by(By.XPATH, "//span[contains(.,'No thanks')]")
+    #selenium.invisible_by(By.XPATH, "//span[contains(.,'No thanks')]")
 
     selenium.screenshot(mode+'-chat')
 
